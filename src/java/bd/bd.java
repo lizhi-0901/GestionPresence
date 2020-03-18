f8bf61f0223d97034bac712c26f42223229ce561
lizhizhang <lizhiwang0901@gmail.com>
2020-03-18 20:21:00 +0100

    20:20

--- src/java/bd/bd.java
+++ src/java/bd/bd.java
@@ -2,6 +2,10 @@ package bd;
 
 
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.HashSet;
+import java.util.Set;
+import metier.Matiere;
 import metier.Personnel;
 import org.hibernate.Transaction;
 import org.hibernate.Session;
@@ -20,6 +24,7 @@ public class bd {
     
      static Session session = HibernateUtil.getSessionFactory().getCurrentSession();
      static Transaction t = session.beginTransaction();
+     SimpleDateFormat df = new SimpleDateFormat("dd-mm-yyyy");
     
      /**
       * c'est une function qui prends en entree comme identifiant 
@@ -36,8 +41,27 @@ public class bd {
           return "mdp";
     }
     
+    /**
+     * 
+     * @param libelleFormation
+     * @return afficher la liste de formation qui est relie avec ce formation
+     */
+    public Set<Matiere> getcours(String libelleFormation){
+          Set<Matiere> setcours =new HashSet<>();
+          return setcours;
+    }
     
+    /**
+     * 
+     * @param idgroupe
+     * @return retourne la liste etudiant qui est dans ce group TD
+     */
+    public Set<Personnel> getEtudiant(String idgroupe){
+           Set<Personnel> setetudiant =new HashSet<>();
+           return setetudiant;
+    }
     
+   
     
    
 }
